84ea1704e5a5d2f4d18d17b357df22bec6efb75b
Patrick Dolan <dolanp1992@gmail.com>
2022-04-07 19:23:46 -0700

    remove empty spaces

--- PierresMarket/Models/Treat.cs
+++ PierresMarket/Models/Treat.cs
@@ -13,8 +13,6 @@ namespace PierresMarket.Models
     public string Description { get; set; }
     public int Price { get; set; }
     public virtual ApplicationUser User { get; set; }
-
     public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
-
   }
 }
