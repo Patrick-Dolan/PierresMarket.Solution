fd2d4641001949cbfb50e97febdc86f9552b3b81
Patrick Dolan <dolanp1992@gmail.com>
2022-04-07 18:05:18 -0700

    add splash page showing all treats and flavors on home

--- PierresMarket/Controllers/HomeController.cs
+++ PierresMarket/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using PierresMarket.Models;
+using System.Linq;
 
 namespace PierresMarket.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresMarketContext _db;
+    public HomeController(PierresMarketContext db)
+    {
+      _db = db;
+    }
     public ActionResult Index()
     {
-      return View();
+      ViewBag.Flavors = _db.Flavors.ToList();
+      return View(_db.Treats.ToList());
     }
   }
 }
