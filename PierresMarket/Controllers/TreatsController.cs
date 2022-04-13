363a7cbdb7893345ab0e38731c83b9725458d632
Patrick Dolan <dolanp1992@gmail.com>
2022-04-13 10:10:00 -0700

    add treat images to to create system for treats

--- PierresMarket/Controllers/TreatsController.cs
+++ PierresMarket/Controllers/TreatsController.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PierresMarket.Models;
+using System;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -14,11 +17,13 @@ namespace PierresMarket.Controllers
   public class TreatsController : Controller
   {
     private readonly PierresMarketContext _db;
+    private readonly IWebHostEnvironment _hostEnvironment;
     private readonly UserManager<ApplicationUser> _userManager;
-    public TreatsController(UserManager<ApplicationUser> userManager, PierresMarketContext db)
+    public TreatsController(UserManager<ApplicationUser> userManager, PierresMarketContext db, IWebHostEnvironment hostEnvironment)
     {
       _userManager = userManager;
       _db = db;
+      _hostEnvironment = hostEnvironment;
     }
 
     [AllowAnonymous]
@@ -39,6 +44,16 @@ namespace PierresMarket.Controllers
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
       treat.User = currentUser;
+      //Image info
+      string wwwRootPath = _hostEnvironment.WebRootPath;
+      string fileName = Path.GetFileNameWithoutExtension(treat.ImageFile.FileName);
+      string extention = Path.GetExtension(treat.ImageFile.FileName);
+      treat.ImageName = fileName = fileName + DateTime.Now.ToString("yymmssffff") + extention;
+      string path = Path.Combine(wwwRootPath + "/img/TreatImages/", fileName);
+      using (var fileStream = new FileStream(path, FileMode.Create))
+      {
+        await treat.ImageFile.CopyToAsync(fileStream);
+      }
       _db.Treats.Add(treat);
       _db.SaveChanges();
       if(FlavorId != 0)
