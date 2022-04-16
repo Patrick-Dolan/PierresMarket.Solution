99b25709fe604adbc646c9b789f1f1fe880094e3
Patrick Dolan <dolanp1992@gmail.com>
2022-04-16 10:22:03 -0700

    add migrations for profile images

---
+++ PierresMarket/Migrations/20220416161847_UserProfilePicture.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierresMarket.Migrations
+{
+    public partial class UserProfilePicture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "AspNetUsers",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "AspNetUsers");
+        }
+    }
+}
