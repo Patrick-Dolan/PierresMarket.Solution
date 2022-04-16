b4db5ff4b2d007e000f22cb8c752a77e98a0dac8
Patrick Dolan <dolanp1992@gmail.com>
2022-04-16 08:16:16 -0700

    add display name for username

--- PierresMarket/ViewModels/LoginViewModel.cs
+++ PierresMarket/ViewModels/LoginViewModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace PierresMarket.ViewModels
@@ -5,6 +6,7 @@ namespace PierresMarket.ViewModels
   public class LoginViewModel
   {
     [Required]
+    [Display(Name = "Username")]
     public string UserName { get; set; }
     [Required]
     public string Password { get; set; }
